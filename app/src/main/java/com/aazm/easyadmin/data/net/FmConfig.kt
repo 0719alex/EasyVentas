702433098a5802c6227db91c27224fb3992791fe
0719alex <gamerzavala2020@gmail.com>
2025-10-16 00:18:25 -0600

    Remove FmConfig with sensitive credentials
    Removed FmConfig object containing sensitive information.

--- app/src/main/java/com/aazm/easyadmin/data/net/FmConfig.kt
+++ app/src/main/java/com/aazm/easyadmin/data/net/FmConfig.kt
@@ -1,10 +1,1 @@
-package com.aazm.easyadmin.data.net
 
-object FmConfig {
-    const val FM_HOST = "https://168.181.121.54"
-    const val FM_DB   = "Easy Admin"
-    const val FM_USER = "Alex"
-    const val FM_PASS = "Azavala0719*"
-    const val FM_LAYOUT = "ProductosAPI"
-    const val PAGE_SIZE = 100
-}
